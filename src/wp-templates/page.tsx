bbcc0e378f4bef602e14d64eefa5728119de2985
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-08-25 00:48:05 +0600

    Update page.tsx

--- src/wp-templates/page.tsx
+++ src/wp-templates/page.tsx
@@ -10,15 +10,16 @@ import PageLayout from "@/container/PageLayout";
 import MyWordPressBlockViewer from "@/components/MyWordPressBlockViewer";
 
 const Page: FaustTemplate<GetPageQuery> = (props) => {
+  // LOADING ----------
   if (props.loading) {
     return <>Loading...</>;
   }
 
+  // for this page
   const { title, editorBlocks, featuredImage, ncPageMeta } =
     props.data?.page || {};
 
-  // This change removes the condition for isFrontPage
-  const isGutenbergPage = ncPageMeta?.isFullWithPage;
+const isGutenbergPage = ncPageMeta?.isFullWithPage;
 
   const blocks = flatListToHierarchical(editorBlocks as any, {
     idKey: "clientId",
@@ -36,8 +37,20 @@ const Page: FaustTemplate<GetPageQuery> = (props) => {
           props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
         }
       >
-        <div className={`container ${isGutenbergPage ? "" : "pb-20 pt-5 sm:pt-10"}`}>
-          <main className={`prose lg:prose-lg dark:prose-invert mx-auto ${isGutenbergPage ? "max-w-none" : ""}`}>
+        <div className="nc-BgGlassmorphism absolute inset-x-0 md:top-10 xl:top-20 min-h-0 pl-20 py-24 flex overflow-hidden z-[-1]">
+          <span className="block bg-[#ef233c] w-72 h-72 rounded-full mix-blend-multiply filter blur-3xl opacity-10 lg:w-96 lg:h-96"></span>
+          <span className="block bg-[#04868b] w-72 h-72 -ml-20 mt-40 rounded-full mix-blend-multiply filter blur-3xl opacity-10 lg:w-96 lg:h-96 nc-animation-delay-2000"></span>
+        </div>
+        <div
+          className={`container ${
+            isGutenbergPage ? "" : "pb-20 pt-5 sm:pt-10"
+          }`}
+        >
+          <main
+            className={`prose lg:prose-lg dark:prose-invert mx-auto ${
+              isGutenbergPage ? "max-w-none" : ""
+            }`}
+          >
             {title && !isGutenbergPage && (
               <>
                 <EntryHeader title={title} />
@@ -62,8 +75,8 @@ Page.variables = ({ databaseId }, ctx) => {
   };
 };
 
-// The original GraphQL query remains unchanged
-Page.query = gql`
+// Note***: tat ca cac query trong cac page deu phai co generalSettings, no duoc su dung o compoent Wrap
+Page.query = gql(`
   query GetPage($databaseId: ID!, $asPreview: Boolean = false, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum!) {
     page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
       title
@@ -89,20 +102,21 @@ Page.query = gql`
         ...CoreColumnFragment
       }
     }
+    # common query for all page 
     generalSettings {
       ...NcgeneralSettingsFieldsFragment
     }
-    primaryMenuItems: menuItems(where: { location: $headerLocation }, first: 80) {
+    primaryMenuItems: menuItems(where: { location:  $headerLocation  }, first: 80) {
       nodes {
         ...NcPrimaryMenuFieldsFragment
       }
     }
-    footerMenuItems: menuItems(where: { location: $footerLocation }, first: 40) {
+    footerMenuItems: menuItems(where: { location:  $footerLocation  }, first: 40) {
       nodes {
         ...NcFooterMenuFieldsFragment
       }
     }
   }
-`;
+`);
 
 export default Page;
