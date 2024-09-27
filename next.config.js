04d5b070ecc52c9dc4a2013f2c120bf9e4dec433
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-09-27 23:26:03 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,49 @@
+const { withFaust } = require('@faustwp/core');
+const { createSecureHeaders } = require('next-secure-headers');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['forestgreen-pig-914075.hostingersite.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'", 'https:'],
+              styleSrc: ["'self'", "'unsafe-inline'", 'https:'],
+              scriptSrc: ["'self'", "'unsafe-eval'", "'unsafe-inline'", 'https:'],
+              imgSrc: ["'self'", 'data:', 'https:'],
+              connectSrc: ["'self'", 'https:'],
+              frameSrc: [process.env.NEXT_PUBLIC_WORDPRESS_URL, 'https:'],
+              fontSrc: ["'self'", 'https:'],
+            },
+          },
+          referrerPolicy: 'strict-origin-when-cross-origin',
+        }),
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:slug',
+        destination: '/post/:slug',
+      },
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+      {
+        source: '/rss.xml',
+        destination: '/api/rss',
+      },
+    ];
+  },
+};
+
+module.exports = withFaust(nextConfig);
