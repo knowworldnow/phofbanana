15de5964ad83b080c88fed377428cd432559fb90
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-10-14 23:29:51 +0600

    Delete src/app/sitemap.xml directory

--- src/app/sitemap.xml/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const sitemapUrl = `${process.env.NEXT_PUBLIC_URL}/api/sitemap`
-  return NextResponse.redirect(sitemapUrl)
-}
