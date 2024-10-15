1ac617f4acfe5f1cab29b35ec1836903d50e2d03
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-10-15 20:22:39 +0600

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,25 @@
-const SITE_URL = process.env.NEXT_PUBLIC_URL;
+/** @type {import('next-sitemap').IConfig} */
+
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://phofbanana.com'
 
 module.exports = {
   siteUrl: SITE_URL,
-  generateRobotsTxt: false,
-  exclude: ['/api/*'],
-  outDir: 'public',
-  sitemapSize: 5000,
-  additionalPaths: async (config) => {
-    return [{ loc: '/server-sitemap.xml', priority: 0.7, changefreq: 'daily' }];
+  generateRobotsTxt: true,
+  exclude: ['/wordpress-sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${SITE_URL}/wordpress-sitemap.xml`,
+    ],
   },
-};
+  transform: (config, path) => {
+    // Exclude date-based archive pages
+    if (path.match(/\/\d{4}\/\d{2}\/\d{2}\/.*/gim)) {
+      return null
+    }
+
+    return {
+      loc: path,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    }
+  },
+}
