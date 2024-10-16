86ce1926a7a752cee449b2de0da7ba74bf94398e
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-10-17 00:02:50 +0600

    Create BananaAd.tsx

---
+++ src/components/BananaAd.tsx
@@ -0,0 +1,12 @@
+export default function BananaAd() {
+  return (
+    <div className="my-8">
+      <ins className="adsbygoogle"
+           style={{ display: 'block' }}
+           data-ad-client="ca-pub-9144697979680971"
+           data-ad-slot="5167818423"
+           data-ad-format="auto"
+           data-full-width-responsive="true"></ins>
+    </div>
+  );
+}
