bae535dd0afc2cd300a2e262116441834ce92b3e
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-10-17 01:45:44 +0600

    Update Seo.tsx

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -25,44 +25,40 @@ const SEO: React.FC<SEOProps> = ({
   author,
   siteName,
 }) => {
+  const formattedCanonicalUrl = canonicalUrl.replace(/\/$/, '');
+
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="canonical" href={canonicalUrl} />
+      <link rel="canonical" href={formattedCanonicalUrl} />
       
-      {/* Open Graph / Facebook */}
       <meta property="og:type" content={ogType} />
-      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:url" content={formattedCanonicalUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
       <meta property="og:image:alt" content={ogImageAlt} />
       <meta property="og:site_name" content={siteName} />
       
-      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:url" content={canonicalUrl} />
+      <meta name="twitter:url" content={formattedCanonicalUrl} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
       <meta name="twitter:image:alt" content={ogImageAlt} />
       
-      {/* LinkedIn */}
       <meta property="linkedin:title" content={title} />
       <meta property="linkedin:description" content={description} />
       <meta property="linkedin:image" content={ogImage} />
       
-      {/* Pinterest */}
       <meta name="pinterest:title" content={title} />
       <meta name="pinterest:description" content={description} />
       <meta name="pinterest:image" content={ogImage} />
       
-      {/* WhatsApp */}
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       
-      {/* Article specific (if applicable) */}
       {ogType === 'article' && (
         <>
           {publishedTime && <meta property="article:published_time" content={publishedTime} />}
@@ -71,13 +67,11 @@ const SEO: React.FC<SEOProps> = ({
         </>
       )}
       
-      {/* Additional tags for better indexing and sharing */}
       <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       
-      {/* Schema.org markup for Google+ */}
       <meta itemProp="name" content={title} />
       <meta itemProp="description" content={description} />
       <meta itemProp="image" content={ogImage} />
