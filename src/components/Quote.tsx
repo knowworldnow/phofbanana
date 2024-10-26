baf0500f4e35ee956c531f7b6f4cfe23055004ef
knowworldnow <46709465+knowworldnow@users.noreply.github.com>
2024-10-26 22:27:22 +0600

    Update Quote.tsx

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -6,7 +6,7 @@ interface QuoteProps {
 
 export function Quote({ children }: QuoteProps) {
   return (
-    <blockquote className="wp-block-quote border-l-4 pl-4 py-2 my-4 italic text-gray-700 dark:text-gray-300">
+    <blockquote className="quote-box">
       {children}
     </blockquote>
   );
